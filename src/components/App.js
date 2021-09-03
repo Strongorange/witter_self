6dd4d6469f1e5a136a8219a68b39a4560b08cd08
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-09-03 23:18:54 +0900

    Fix Initializing

--- src/components/App.js
+++ src/components/App.js
@@ -2,42 +2,43 @@ import React, { useEffect, useState } from "react";
 import Router from "./Router";
 import { authService } from "../fbase";
 import { useSetIsLoggedIn, useSetUser } from "../context";
+import GlobalStyle from "./GlobalStyle";
 
 const App = () => {
   const setIsLoggedIn = useSetIsLoggedIn();
-  const [initializing, setInitializing] = useState(true);
+  const [init, setInit] = useState(false);
   const setUser = useSetUser();
 
   useEffect(() => {
-    try {
-      authService.onAuthStateChanged((user) => {
-        if (user) {
-          if (user.displayName === null) {
-            console.log("null user", user);
-            const index = user.email.indexOf("@");
-            const tempName = user.email.substring(0, index);
-            user.updateProfile({
-              displayName: tempName,
-            });
-          }
-          setUser({
-            displayName: user.displayName,
-            uid: user.uid,
-            updateProfile: (args) => user.updateProfile(args),
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        if (user.displayName === null) {
+          console.log("null user", user);
+          const index = user.email.indexOf("@");
+          const tempName = user.email.substring(0, index);
+          user.updateProfile({
+            displayName: tempName,
           });
-          setIsLoggedIn(true);
-        } else {
-          setIsLoggedIn(false);
         }
-      });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setInitializing(false);
-    }
+        setUser({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
   }, []);
 
-  return <>{initializing ? "Initializing" : <Router />}</>;
+  return (
+    <>
+      <GlobalStyle />
+      {init ? <Router /> : "Initializing"}
+    </>
+  );
 };
 
 export default App;
